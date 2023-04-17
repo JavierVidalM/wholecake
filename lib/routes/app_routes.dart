35319d603af02eae2c3f1086d5d06aed71f09058
JavierVidalM <102665760+JavierVidalM@users.noreply.github.com>
2023-04-16 21:13:49 -0400

    Creacion de las rutas en "Routes", creacion de algunsa de las carpetas necesarias para el proyecto

---
+++ lib/routes/app_routes.dart
@@ -0,0 +1,44 @@
+import 'package:wholecake/views/pages.dart';
+import 'package:flutter/material.dart';
+
+class AppRoutes{
+    static const initialRoute = 'LoginMain';
+
+    static Map<String,Widget Function(BuildContext)> routes = {
+
+        //Pantallas del Login
+        'LoginMain' : (BuildContext context) => const LoginMain(),
+        'LoginUser' : (BuildContext context) => const LoginUser(),
+        'SigninUser' : (BuildContext context) => const SigninUser(),
+        
+        //Pantalla de inicio
+        'MainPage' : (BuildContext context) => const MainPage(),
+
+        'Inventory' : (BuildContext context) => const Inventory(),
+        'Inputs' : (BuildContext context) => const Supplies(),
+        'ToExpire' ; (BuildContext context) => const ToExpire(),
+
+        //Pantallas de producto
+        'ProductsView' : (BuildContext context) => const ProductsView(),
+        'ProductsAdd' : (BuildContext context) => const ProductsAdd(),
+        'ProductsEdit' : (BuildContext context) => const ProductsEdit(),
+        
+        'ProvidersAdd' : (BuildContext context) => const ProvidersAdd(),
+        'ProvidersView' : (BuildContext context) => const ProvidersView(),
+        'InputsRecipt' : (BuildContext context) => const InputsRecipt(),
+        'PurchaseOrders' : (BuildContext context) => const PurchaseOrders(),
+        'PurchaseList' : (BuildContext context) => const PurchaseList(),
+
+        //Pantallas de ventas
+        'SellsView' : (BuildContext context) => const SellsView(),
+        'SellsDetail' : (BuildContext context) => const SellsDetail(),
+    
+        //Pantalla de error
+        'Error' : (BuildContext context) => const ErrorPage(),
+    };
+
+    static Route<dynamic> onGenerateRoute(RouteSettings settings){
+        return MaterialPageRoute(
+            builder: (context) => const ErrorPage(),);
+    }
+}
