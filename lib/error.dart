2e0d695c62085a6b5e20a62671d3911f86bf74a2
JavierVidalM <javier.vidalm@outlook.com>
2023-04-17 21:47:13 -0400

    Agregada la pagina de error

---
+++ lib/error.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+class ErrorPage extends StatelessWidget {
+  const ErrorPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFBDE0FE),
+      appBar: AppBar(
+        backgroundColor: const Color(0xFFFFC4E3),
+        title: Container(
+          alignment: Alignment.center,
+          child: Image.asset('assets/images/logo_SW.png'),
+          height: kToolbarHeight * 0.9,
+        ),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const SizedBox(height: 25),
+          Image.asset(
+            'assets/images/error_SW.png',
+            errorBuilder: (BuildContext context, Object exception,
+                StackTrace? stackTrace) {
+              print('Error al cargar la imagen: $exception');
+              return const Text('Error al cargar la imagen');
+            },
+          ),
+          const SizedBox(height: 20),
+          const Text(
+            'Ups, aquí no hay nada',
+            style: TextStyle(
+                fontSize: 26,
+                fontWeight: FontWeight.bold,
+                color: Color(0xFF2C2C2C)),
+          ),
+          const SizedBox(height: 10),
+          const Text(
+            'Por favor intente más tarde,\n si el problema persiste contacte con un administrador',
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 20, color: Color(0xFF2C2C2C)),
+          ),
+          const SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: () {},
+            style: ElevatedButton.styleFrom(
+              primary: const Color(0xFFFFB5D7),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+            ),
+            child: Container(
+              width: 150,
+              height: 50,
+              alignment: Alignment.center,
+              child: const Text(
+                'Reintentar',
+                style: TextStyle(color: Color(0xFF5D2A42)),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/views/views.dart
+++ lib/views/views.dart
@@ -1,7 +1,11 @@
+//Modulo Login
 export 'package:wholecake/views/login/login_main.dart';
 export 'package:wholecake/views/login/login_user.dart';
 export 'package:wholecake/views/login/login_signin.dart';
+
+//Modulo Inicio
 export 'package:wholecake/views/main/main_page.dart';
+
 export 'package:wholecake/views/Inventory.dart';
 export 'package:wholecake/views/Supplies.dart';
 export 'package:wholecake/views/ToExpire.dart';
