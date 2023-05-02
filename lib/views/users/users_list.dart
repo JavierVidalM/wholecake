39ce246b55f382b68c564990920ea1c7de89a0cc
Miguel Quiroz <miguel881@live.cl>
2023-05-01 22:47:38 -0400

    users list
    list

--- lib/views/users/users_list.dart
+++ lib/views/users/users_list.dart
@@ -1,71 +1,99 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+class UsersList extends StatefulWidget {
+  const UsersList({Key? key}) : super(key: key);
 
-class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Listado de usuarios',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: UserListPage(),
-    );
-  }
+  _UsersListState createState() => _UsersListState();
 }
 
-class UserListPage extends StatelessWidget {
+class _UsersListState extends State<UsersList> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Usuarios', style: TextStyle(color: Colors.black)),
-        backgroundColor: Colors.white,
-        centerTitle: true,
+        title: const Text('Listado de usuarios'),
+        backgroundColor: Color(0xFFBDE0FE),
       ),
-      body: ListView(
-        children: [
-          UserCard(),
-          UserCard(),
-          UserCard(),
-        ],
-      ),
-    );
-  }
-}
-
-class UserCard extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      margin: const EdgeInsets.all(16),
-      child: Row(
-        children: [
-          Container(
-            color: Color(0xffbde0fe),
-            height: 100,
-            width: 100,
-            child: Image.asset('assets/user.png'),
-          ),
-          Expanded(
+      body: ListView.builder(
+        // itemCount: list.listadousers.length,
+        itemBuilder: (context, index) {
+          // final users = list.listadousers[index];
+          return SizedBox(
+            width: MediaQuery.of(context).size.width * 0.85,
+            height: 120,
             child: Card(
-              color: Color(0xffffc4e3),
-              margin: const EdgeInsets.all(0),
-              child: Padding(
-                padding: const EdgeInsets.all(16),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text('Dou', style: TextStyle(fontSize: 24)),
-                    SizedBox(height: 8),
-                    Text('Panadero', style: TextStyle(fontSize: 18)),
-                  ],
-                ),
+              color: const Color(0xFFBDE0FE),
+              child: Row(
+                children: [
+                  Container(
+                    width: 100,
+                    height: 100,
+                    margin: const EdgeInsets.all(10),
+                    decoration: const BoxDecoration(
+                      shape: BoxShape.circle,
+                      image: DecorationImage(
+                        image:
+                            NetworkImage('https://i.redd.it/bjsvfpvxwuo41.jpg'),
+                        fit: BoxFit.fill,
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Card(
+                      color: const Color(0xFFFFC4E3),
+                      margin: const EdgeInsets.all(10),
+                      child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: const [
+                            Text(
+                              ('Aqui iria name bbdd'),
+                              style: TextStyle(
+                                fontSize: 20,
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold,
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                            ),
+                            SizedBox(height: 10),
+                            Text(
+                              ('aqui va cargo bbdd'),
+                              style: TextStyle(
+                                fontSize: 20,
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold,
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                            ),
+                            SizedBox(height: 10),
+                            Text(
+                              ('aqui va local bbdd'),
+                              style: TextStyle(
+                                fontSize: 20,
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold,
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                            ),
+                            SizedBox(height: 10),
+                            Text(
+                              ('aqui va numero bbdd pura fe '),
+                              style: TextStyle(
+                                fontSize: 20,
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold,
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                            ),
+                          ]),
+                    ),
+                  ),
+                ],
               ),
             ),
-          ),
-        ],
+          );
+        },
       ),
     );
   }
