184714ba622f3696009a418f968cb339c1cf7b70
MigueSlAxl <migueazul122@gmail.com>
2023-05-10 21:24:22 -0400

    pantalla agregar proveedores

--- lib/views/suppliers/suppliers_add.dart
+++ lib/views/suppliers/suppliers_add.dart
@@ -1,156 +1,185 @@
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
+// import 'package:wholecake/services/users_services.dart';
+// import 'package:file_picker/file_picker.dart';
+// import 'package:wholecake/views/utilities/sidebar.dart';
+// import 'package:wholecake/theme/theme.dart';
+// import 'package:wholecake/views/home/home.dart';
+// import 'dart:convert';
+// import 'dart:io';
+// import 'package:wholecake/views/users/users_list.dart';
 
-import 'package:wholecake/views/suppliers/suppliers_add.dart';
+// class SuppliersAdd extends StatelessWidget {
+//   const SuppliersAdd({Key? key}) : super(key: key);
 
-class SuppliersAdd extends StatelessWidget {
-  const SuppliersAdd({Key? key}) : super(key: key);
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       theme: SweetCakeTheme.mainTheme,
+//       title: 'Agregar Proveedor',
+//       home: SuppliersAddPage(),
+//       ); 
+//   }
+// }
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Información del proveedor',
-      theme: ThemeData(
-        primarySwatch: Colors.pink,
-      ),
-      home: const SuppliersAddPagePage(title: 'Información del proveedor'),
-    );
-  }
-}
+// class SuppliersAddPage extends StatefulWidget {
+//   const SuppliersAddPagePage({Key? key }) : super(key: key);
+//   @override
+//   SuppliersAddPageState createState() => SuppliersAddPageState();
+// }
 
-class SuppliersAddPagePage extends StatefulWidget {
-  const SuppliersAddPagePage({Key? key, required this.title}) : super(key: key);
-  final String title;
-  @override
-  State<SuppliersAddPagePage> createState() => SuppliersAddPagePageState();
-}
+// class SuppliersAddPagePageState extends State<SuppliersAddPagePage> {
+//   TextEditingController rutproveedorController = TextEditingController();
+//   TextEditingController nombreproveedorController = TextEditingController();
+//   TextEditingController tipoproductoController = TextEditingController();
+//   TextEditingController correoproveedorController =TextEditingController();
+//   TextEditingController numerotelefonoController = TextEditingController();
 
-class SuppliersAddPagePageState extends State<SuppliersAddPagePage> {
-  TextEditingController rutproveedorController = TextEditingController();
-  TextEditingController nombreproveedorController = TextEditingController();
-  TextEditingController tipoproductoproveedorController =
-      TextEditingController();
-  TextEditingController marcaproductoproveedorController =
-      TextEditingController();
-  TextEditingController correoproveedorController = TextEditingController();
-  TextEditingController costoproductoproveedorController =
-      TextEditingController();
-  TextEditingController numeroproveedorController = TextEditingController();
+//   @override
+//   void dispose(){
+//   rutproveedorController.dispose();
+//   nombreproveedorController.dispose();
+//   tipoproductoController.dispose();
+//   correoproveedorController.dispose();
+//   numerotelefonoController.dispose();
+//   super.dispose();
+//   }
 
-  @override
-  void dispose() {
-    rutproveedorController.dispose();
-    nombreproveedorController.dispose();
-    tipoproductoproveedorController.dispose();
-    marcaproductoproveedorController.dispose();
-    correoproveedorController.dispose();
-    costoproductoproveedorController.dispose();
-    numeroproveedorController.dispose();
-    super.dispose();
-  }
+//   Future<void> _saveData() async{
+//     final msg = jsonEncode({
+//     'rut': rutproveedorController.text,
+//     'nombre_proveedor': nombreproveedorController.text,
+//     'tipo_producto': tipoproductoController.text,
+//     'correo_proveedor': correoproveedorController.text,
+//     'numero_telefono': numerotelefonoController.text,
+//   });
+//     await SuppliersService().SuppliersAdd(msg);
+//     Navigator.push(
+//         context, MaterialPageRoute(builder: (context) => SuppliersList()));
+// }
 
-  void _saveData() {
-    // Aquí es donde guardarías la información en la base de datos
-    print('Nombre: ${rutproveedorController.text}');
-    print('Fecha de elaboración: ${nombreproveedorController.text}');
-    print('Fecha de vencimiento: ${tipoproductoproveedorController.text}');
-    print('Descripción: ${marcaproductoproveedorController.text}');
-    print('Descripción: ${correoproveedorController.text}');
-    print('Descripción: ${costoproductoproveedorController.text}');
-    print('Descripción: ${numeroproveedorController.text}');
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: const Text('Información del proveedor'),
-        ),
-        body: SingleChildScrollView(
-            padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 20),
-            child: Column(
-              children: [
-                InputTextField(
-                  hintText: 'Rut Proveedor',
-                  labelText: 'Rut Proveedor',
-                  controller: rutproveedorController,
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                InputTextField(
-                  hintText: 'Nombre  Proveedor',
-                  labelText: 'Nombre Proveedor',
-                  controller: nombreproveedorController,
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                InputTextField(
-                  hintText: 'Tipo  producto',
-                  labelText: 'Tipo producto',
-                  controller: tipoproductoproveedorController,
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                InputTextField(
-                  hintText: 'Marca  producto',
-                  labelText: 'Marca producto',
-                  controller: marcaproductoproveedorController,
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                InputTextField(
-                  hintText: 'Correo  Proveedor',
-                  labelText: 'Correo Proveedor',
-                  controller: correoproveedorController,
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                InputTextField(
-                  hintText: 'Costo del producto',
-                  labelText: 'Costo del producto',
-                  controller: costoproductoproveedorController,
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                InputTextField(
-                  hintText: 'Número proveedor',
-                  labelText: 'Número proveedor',
-                  controller: numeroproveedorController,
-                ),
-                ElevatedButton(
-                    onPressed: () => Navigator.of(context).pop,
-                    child: const Text('Volver'))
-              ],
-            )));
-  }
-}
-
-class InputTextField extends StatelessWidget {
-  const InputTextField({
-    Key? key,
-    required this.hintText,
-    required this.labelText,
-    required this.controller,
-  }) : super(key: key);
-
-  final String hintText;
-  final String labelText;
-  final TextEditingController controller;
-
-  @override
-  Widget build(BuildContext context) {
-    return TextFormField(
-      decoration: InputDecoration(
-        hintText: hintText,
-        labelText: labelText,
-        border: UnderlineInputBorder(),
-      ),
-      controller: controller,
-    );
-  }
-}
+// @override
+// Widget build(BuildContext context) {
+//   return Scaffold(
+//     appBar: AppBar(
+//       title: Text('Agregar Proveedor',
+//         style: Theme.of(context).textTheme.titleLarge),
+//       toolbarHeight: MediaQuery.of(context).size.height * 0.1,
+//     ),
+//     drawer: const SideBar(),
+//     body: SingleChildScrollView(
+//       padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+//       child: Column(
+//         children: [
+//           Padding(
+//             padding: EdgeInsets.only(
+//               bottom: MediaQuery.of(context).size.height * 0.02,
+//             ),
+//             child: InkWell(
+//               onTap: () async {
+//                 final result = await FilePicker.platform.pickFiles(
+//                   type: FileType.image
+//                 );
+//                 if (result != null) {
+//                   setState(() {
+//                     imagen = File(result.files.single.path!);
+//                       });
+//                 }                
+//               },
+//               child: Container(
+//                   width: 80.0,
+//                   height: 80.0,
+//                   decoration: const BoxDecoration(
+//                     shape: BoxShape.circle,
+//                     color: Color(0xFF909090),
+//                   ),
+//                   child: ClipOval(
+//                     child: imagen != null
+//                         ? Image.file(
+//                             imagen!,
+//                             width: 80.0,
+//                             height: 80.0,
+//                             fit: BoxFit.cover,
+//                           )
+//                         : const Icon(
+//                             Icons.add,
+//                             size: 40.0,
+//                             color: Color(0xFFC0C0C0),
+//                           ),
+//                   ),
+//                 ),  
+//             ),
+//           ),
+//           TextFormField(
+//             controller: nombreproveedorController,
+//             decoration: const InputDecoration(hintText: 'Nombre Proveedor'),
+//           ),
+//           const SizedBox(
+//             height: 20,
+//           ),  
+//           TextFormField(
+//             controller: rutproveedorController,
+//             decoration: const InputDecoration(hintText: 'Rut Proveedor'),
+//           ),
+//           const SizedBox(
+//             height: 20,
+//           ),
+//           TextFormField(
+//             controller: tipoproductoController,
+//             decoration: const InputDecoration(hintText: 'Tipo de Producto'),
+//           ),
+//           const SizedBox(
+//             height: 20,
+//           ),
+//           TextFormField(
+//             controller: correoproveedorController,
+//             decoration: const InputDecoration(hintText: 'Correo Proveedor'),
+//           ),
+//           const SizedBox(
+//             height: 20,
+//           ),
+//           TextFormField(
+//             controller: numerotelefonoController,
+//             decoration: const InputDecoration(hintText: 'Número de Teléfono'),
+//           ),
+//           const SizedBox(
+//             height: 20,
+//           ),
+//           ElevatedButton(
+//           onPressed: () {
+//             _saveData();
+//             Navigator.push(
+//                 context,
+//                 MaterialPageRoute(
+//                     builder: (context) => const HomePage()));
+//           },
+//           style: ElevatedButton.styleFrom(
+//             minimumSize: Size(
+//               (MediaQuery.of(context).size.width * 0.6),
+//               (MediaQuery.of(context).size.height * 0.07),
+//             ),
+//           ),
+//           child: const Text('Guardar'),
+//         ),
+//           const SizedBox(height: 16.0),
+//           ElevatedButton(
+//             onPressed: () {
+//               Navigator.push(
+//                 context,
+//                 MaterialPageRoute(builder: (context) => const HomePage()),
+//               );
+//             },
+//             style: ElevatedButton.styleFrom(
+//               minimumSize: Size(
+//                 (MediaQuery.of(context).size.width * 0.6),
+//                 (MediaQuery.of(context).size.height * 0.07),
+//               ),
+//             ),
+//             child: const Text('Volver'),
+//           ),                                     
+//         ],
+//     ),
+//     ),
+//   );
+// }
+// }
