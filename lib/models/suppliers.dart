1349633dcfbdb2ebde92a0702b5a6748a1ed9bac
Nikolo <sanchez.nicolas.9917@gmail.com>
2023-05-13 22:37:47 -0400

    suppliers.dart update

--- lib/models/suppliers.dart
+++ lib/models/suppliers.dart
@@ -5,68 +5,74 @@
 import 'dart:convert';
 
 class Suppliers {
-    List<ListSup> listSup;
-    Suppliers({
-        required this.listSup,
-    });
+  List<ListSup> listSup;
+  Suppliers({
+    required this.listSup,
+  });
 
-    factory Suppliers.fromJson(String str) => Suppliers.fromMap(json.decode(str));
+  factory Suppliers.fromJson(String str) => Suppliers.fromMap(json.decode(str));
 
-    String toJson() => json.encode(toMap());
+  String toJson() => json.encode(toMap());
 
-    factory Suppliers.fromMap(Map<String, dynamic> json) => Suppliers(
-        listSup: List<ListSup>.from(json["ListSup"].map((x) => ListSup.fromMap(x))),
-    );
+  factory Suppliers.fromMap(Map<String, dynamic> json) => Suppliers(
+        listSup:
+            List<ListSup>.from(json["ListSup"].map((x) => ListSup.fromMap(x))),
+      );
 
-    Map<String, dynamic> toMap() => {
+  Map<String, dynamic> toMap() => {
         "ListSup": List<dynamic>.from(listSup.map((x) => x.toMap())),
-    };
+      };
 }
 
 class ListSup {
-    int supplierId;
-    String nombreProveedor;
-    String rut;
-    String tipoInsumo;
-    String correoProveedor;
-    int telefonoProveedor;
+  int supplierId;
+  String nombreProveedor;
+  String rut;
+  String tipoInsumo;
+  String correoProveedor;
+  int telefonoProveedor;
+  String imagen_insumo;
 
-    ListSup({
-        required this.supplierId,
-        required this.nombreProveedor,
-        required this.rut,
-        required this.tipoInsumo,
-        required this.correoProveedor,
-        required this.telefonoProveedor,
-    });
+  ListSup({
+    required this.supplierId,
+    required this.nombreProveedor,
+    required this.rut,
+    required this.tipoInsumo,
+    required this.correoProveedor,
+    required this.telefonoProveedor,
+    required this.imagen_insumo,
+  });
 
-    factory ListSup.fromJson(String str) => ListSup.fromMap(json.decode(str));
+  factory ListSup.fromJson(String str) => ListSup.fromMap(json.decode(str));
 
-    String toJson() => json.encode(toMap());
+  String toJson() => json.encode(toMap());
 
-    factory ListSup.fromMap(Map<String, dynamic> json) => ListSup(
+  factory ListSup.fromMap(Map<String, dynamic> json) => ListSup(
         supplierId: json["id"],
         nombreProveedor: json["nombre_proveedor"],
         rut: json["rut"],
         tipoInsumo: json["tipo_insumo"],
         correoProveedor: json["correo_proveedor"],
         telefonoProveedor: json["telefono_proveedor"],
-    );
+        imagen_insumo: json["imagen_insumo"]
+      );
 
-    Map<String, dynamic> toMap() => {
+  Map<String, dynamic> toMap() => {
         "supplierId": supplierId,
         "nombre_proveedor": nombreProveedor,
         "rut": rut,
         "tipo_insumo": tipoInsumo,
         "correo_proveedor": correoProveedor,
         "telefono_proveedor": telefonoProveedor,
-    };
-      ListSup copy() => ListSup(
+        "imagen_insumo": imagen_insumo,
+      };
+  ListSup copy() => ListSup(
         supplierId: supplierId,
         nombreProveedor: nombreProveedor,
-        rut:rut,
+        rut: rut,
         tipoInsumo: tipoInsumo,
         correoProveedor: correoProveedor,
         telefonoProveedor: telefonoProveedor,
+        imagen_insumo: imagen_insumo,
       );
 }
