4abb31a50744683612c769711e8dfd6fe53df5ac
Nikolo <sanchez.nicolas.9917@gmail.com>
2023-05-14 21:40:43 -0400

    crud de orden de compra listo en el backend

--- lib/services/productos_services.dart
+++ lib/services/productos_services.dart
@@ -188,6 +188,7 @@ class ProductService extends ChangeNotifier {
         'Basic ' + base64Encode(utf8.encode('$APIUSER:$APIPASS'));
     final response = await http.get(url, headers: {'authorization': basicAuth});
     final suppliersMap = Suppliers.fromJson(response.body);
+    print(response.body);
     listadosuppliers = suppliersMap.listSup;
     isLoading = false;
     notifyListeners();
@@ -257,10 +258,59 @@ class ProductService extends ChangeNotifier {
     String basicAuth =
         'Basic ' + base64Encode(utf8.encode('$APIUSER:$APIPASS'));
     final response = await http.get(url, headers: {'authorization': basicAuth});
-    final OrdenMap = OrdenDeCompa.fromJson(response.body);
-    print(response.body);
-    listaOrdenes = OrdenMap.listOdc;
+    final ordenMap = OrdenDeCompa.fromJson(response.body);
+    listaOrdenes = ordenMap.listOdc;
     isLoading = false;
     notifyListeners();
+  }
+  Future addOrdenCompra(String msg) async {
+    notifyListeners();
+    final url = Uri.http(
+      BASEURL,
+      'ordendc/ordendc_ordendc_add_rest/',
+    );
+    String basicAuth =
+        'Basic ' + base64Encode(utf8.encode('$APIUSER:$APIPASS'));
+    final response = await http.post(url, body: msg, headers: {
+      'authorization': basicAuth,
+      'Content-Type': 'application/json; charset=UTF-8',
+    });
+    final decodeResp = response.body;
+    final ListOdc odc = ListOdc.fromJson(decodeResp);
+    listaOrdenes.add(odc);
+    // notifyListeners();
+    await loadProductos(); //
+    isEditCreate = false;
+  }
+  Future<String> updateOrdenCompra(ListOdc odc) async {
+    final url = Uri.http(
+      BASEURL,
+      'ordendc/ordendc_ordendc_update_rest/',
+    );
+    String basicAuth =
+        'Basic ' + base64Encode(utf8.encode('$APIUSER:$APIPASS'));
+    final response = await http.post(url, body: odc.toJson(), headers: {
+      'authorization': basicAuth,
+      'Content-Type': 'application/json; charset=UTF-8',
+    });
+    final decodeResp = response.body;
+    //actualizamos el listado
+    // final index = listadoproductos
+    //     .indexWhere((element) => element.productoId == product.productoId);
+    // listadoproductos[index] = product;
+    return '';
+  }
+  deleteOrdenCompra(String msg) async {
+    final url = Uri.http(
+      BASEURL,
+      'ordendc/ordendc_ordendc_delete_rest/',
+    );
+    String basicAuth =
+        'Basic ' + base64Encode(utf8.encode('$APIUSER:$APIPASS'));
+    final response = await http.post(url, body: msg, headers: {
+      'authorization': basicAuth,
+      'Content-Type': 'application/json; charset=UTF-8',
+    });
+    final decodeResp = response.body;
   }
 }
