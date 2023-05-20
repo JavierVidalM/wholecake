160e7dd9d7fa8122ee004c23979ad7bcfa136885
Emperorz0rk <felipe.guajardo.012@gmail.com>
2023-05-20 02:50:03 -0400

    YA MAQUINAS QLAS AVANCE 1
    Hice la pagina de Purchase List donde van a estar las Ordenes de compra, pero necesito ayuda en back y no le puse filtro de momento y esta lista la de purchase orders me falta modificarle algunas cosas pero estan listas para ir probando, eso besos en el poto

--- lib/views/suppliers/suppliers_purchase_list.dart
+++ lib/views/suppliers/suppliers_purchase_list.dart
@@ -1,41 +1,248 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:wholecake/models/ordendecompra.dart';
+import 'package:wholecake/models/productos.dart';
+import 'package:wholecake/models/suppliers.dart';
+import 'package:wholecake/models/categoria.dart';
+import 'package:wholecake/models/suppliers.dart';
+import 'package:wholecake/services/productos_services.dart';
+import 'package:wholecake/theme/theme.dart';
+import 'package:wholecake/views/products/products_filter_view.dart';
+import 'package:wholecake/views/suppliers/suppliers.dart';
+import 'package:wholecake/views/utilities/sidebar.dart';
+import 'package:wholecake/views/utilities/loading_screen.dart';
+import 'package:wholecake/views/suppliers/suppliers_add.dart';
+import 'package:wholecake/views/suppliers/suppliers_edit.dart';
+import 'dart:typed_data';
+import 'package:intl/intl.dart';
+
+import '../../models/categoria.dart';
 
 class PurchaseList extends StatefulWidget {
+  const PurchaseList({Key? key}) : super(key: key);
+
   @override
   _PurchaseListState createState() => _PurchaseListState();
 }
 
 class _PurchaseListState extends State<PurchaseList> {
-  String nombre = '';
+  final int? _selectedCategory = null;
+  Future<String?> filterPopup(ProductService listacat) => showDialog<String>(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: const Text("Filtro"),
+          content: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(
+                    top: MediaQuery.of(context).size.height * 0.02,
+                    bottom: MediaQuery.of(context).size.height * 0.01,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          actions: [
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: ElevatedButton(
+                onPressed: () {},
+                child: const Text(
+                  "Filtrar",
+                  style: TextStyle(fontSize: 16),
+                ),
+              ),
+            ),
+          ],
+        ),
+      );
+
+  Future<void> deletePopup(int ordenId, ListOdc) async {
+    await showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: const Text(
+          "¿Estás seguro de que deseas eliminar la Orden de Compra?",
+          textAlign: TextAlign.center,
+        ),
+        content:
+            const Text("Esta acción no podrá deshacerse una vez completada."),
+        actions: [
+          TextButton(
+            onPressed: () => Navigator.of(context).pop(),
+            child: const Text(
+              "Cancelar",
+              style: TextStyle(fontSize: 18),
+            ),
+          ),
+          TextButton(
+            onPressed: () async {
+              Navigator.of(context).pop();
+              final msg = jsonEncode({
+                'id': ordenId,
+              });
+              await ProductService().deleteOrdenCompra(msg);
+              setState(() {
+                ListOdc.removeWhere(
+                    (OrdenDeCompa) => OrdenDeCompa.ordenId == ordenId);
+              });
+            },
+            child: const Text(
+              "Eliminar",
+              style: TextStyle(color: Colors.red, fontSize: 18),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Cambiar Nombre'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Nombre actual:',
-            ),
-            Text(
-              nombre,
-              style: Theme.of(context).textTheme.headline4,
-            ),
-            SizedBox(height: 20.0),
-            ElevatedButton(
-              onPressed: () {
-                // Aquí puedes agregar la lógica para cambiar el nombre
-                setState(() {
-                  nombre = 'Nuevo Nombre';
-                });
-              },
-              child: Text('Cambiar nombre'),
-            ),
-          ],
+    final listadoView = Provider.of<ProductService>(context);
+    if (listadoView.isLoading) return const LoadingScreen();
+    final List<ListOdc> prod = listadoView.listaOrdenes;
+    final listacat = Provider.of<ProductService>(context);
+
+    return ChangeNotifierProvider(
+      create: (_) => ProductService(),
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'Ordenes de Compra',
+            style: Theme.of(context).textTheme.titleLarge,
+          ),
+          toolbarHeight: MediaQuery.of(context).size.height * 0.1,
+        ),
+        drawer: const SideBar(),
+        body: Consumer<ProductService>(
+          builder: (context, listado, child) {
+            return Column(
+              children: [
+                Container(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: MediaQuery.of(context).size.width * 0.03,
+                    vertical: MediaQuery.of(context).size.height * 0.01,
+                  ),
+                  child: Row(
+                    children: [
+                      IconButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const PurchaseOrders(),
+                            ),
+                          );
+                        },
+                        icon: const Icon(Icons.add),
+                      ),
+                    ],
+                  ),
+                ),
+                Divider(height: MediaQuery.of(context).size.height * 0.005),
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: listado.listaOrdenes.length,
+                    itemBuilder: (context, index) {
+                      final listaOdc = listado.listaOrdenes[index];
+                      String ordenOdc = '';
+                      return Card(
+                        child: Padding(
+                          padding: const EdgeInsets.all(12),
+                          child: Row(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Container(
+                                width: 100,
+                                height: 100,
+                                margin: EdgeInsets.only(
+                                  right:
+                                      MediaQuery.of(context).size.width * 0.03,
+                                  top:
+                                      MediaQuery.of(context).size.height * 0.01,
+                                  bottom:
+                                      MediaQuery.of(context).size.height * 0.01,
+                                ),
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(100),
+                                  shape: BoxShape.rectangle,
+                                ),
+                              ),
+                              Expanded(
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        Expanded(
+                                          child: Text(
+                                            listaOdc.proveedor as String,
+                                            maxLines: 2,
+                                            overflow: TextOverflow.ellipsis,
+                                          ),
+                                        ),
+                                        Row(
+                                          children: [
+                                            IconButton(
+                                              onPressed: () {
+                                                listadoView.selectedSupplier =
+                                                    listado
+                                                        .listadosuppliers[index]
+                                                        .copy();
+                                                Navigator.push(
+                                                  context,
+                                                  MaterialPageRoute(
+                                                    builder: (context) =>
+                                                        SuppliersEdit(),
+                                                  ),
+                                                );
+                                              },
+                                              icon: Icon(Icons.edit),
+                                            ),
+                                            IconButton(
+                                              onPressed: () async {
+                                                deletePopup(
+                                                  listaOdc.ordenId,
+                                                  listado.listaOrdenes,
+                                                );
+                                              },
+                                              icon: Icon(Icons.delete),
+                                            ),
+                                          ],
+                                        ),
+                                      ],
+                                    ),
+                                    SizedBox(height: 10),
+                                    Text(
+                                      'Orden ID: ${listaOdc.ordenId.toString().padRight(10)}',
+                                    ),
+                                    SizedBox(height: 5),
+                                    Text(
+                                      'Fecha: ${listaOdc.fecha.toString().padRight(10)}',
+                                    ),
+                                    SizedBox(height: 10),
+                                    Text(
+                                      'Proveedor: ${listaOdc.proveedor.toString().padRight(10)}',
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ],
+            );
+          },
         ),
       ),
     );
--- lib/views/suppliers/suppliers_purchase_orders.dart
+++ lib/views/suppliers/suppliers_purchase_orders.dart
@@ -1,153 +1,161 @@
-import 'package:date_time_picker/date_time_picker.dart';
+import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:wholecake/models/ordendecompra.dart';
-import 'package:wholecake/models/suppliers.dart';
-import 'package:wholecake/views/home/home.dart';
+import 'package:wholecake/views/suppliers/suppliers.dart';
+import 'package:wholecake/views/suppliers/suppliers_view.dart';
 import 'package:wholecake/views/utilities/sidebar.dart';
 
-import '../../models/categoria.dart';
-import '../../theme/theme_constant.dart';
+import '../../services/productos_services.dart';
 
 class PurchaseOrders extends StatefulWidget {
+  const PurchaseOrders({Key? key}) : super(key: key);
+
   @override
   _PurchaseOrdersState createState() => _PurchaseOrdersState();
 }
 
 class _PurchaseOrdersState extends State<PurchaseOrders> {
-  TextEditingController suppliersController = TextEditingController();
-  String dropdownValue = 'Selecciona un proveedor';
+  TextEditingController ordenIdController = TextEditingController();
+  TextEditingController fechaController = TextEditingController();
+  TextEditingController cantidadController = TextEditingController();
+  TextEditingController proveedorController = TextEditingController();
+  TextEditingController costotalController = TextEditingController();
 
   @override
   void dispose() {
-    suppliersController.dispose();
+    ordenIdController.dispose();
+    fechaController.dispose();
+    cantidadController.dispose();
+    proveedorController.dispose();
+    costotalController.dispose();
     super.dispose();
+  }
+
+  Future<void> _saveData() async {
+    final msg = jsonEncode({
+      'Id': ordenIdController.text,
+      'Fecha': fechaController.text,
+      'Cantidad': cantidadController.text,
+      'Proveedor': proveedorController.text,
+      'Costo Total': costotalController.text,
+    });
+    await ProductService().addOrdenCompra(msg);
+    print('saved');
+    Navigator.push(
+        context, MaterialPageRoute(builder: (context) => SuppliersView()));
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(
-          'Ordenes de compra',
-          style: Theme.of(context).textTheme.headline6,
+        title: const Text(
+          'Crear Orden de Compra',
+          style: TextStyle(
+            color: Color(0xFF5D2A42),
+            fontSize: 24,
+          ),
         ),
-        toolbarHeight: MediaQuery.of(context).size.height * 0.1,
+        backgroundColor: const Color(0xFFFFB5D7),
+        centerTitle: true,
+        titleSpacing: 0,
       ),
-      drawer: SideBar(),
+      drawer: const SideBar(),
       body: SingleChildScrollView(
-        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
         child: Column(
           children: [
-            Padding(
-              padding: EdgeInsets.only(
-                  top: MediaQuery.of(context).size.height * 0.02),
-              child: Theme(
-                data: SweetCakeTheme.calendarTheme,
-                child: DateTimePicker(
-                  dateHintText: 'Fecha',
-                  firstDate: DateTime.now(),
-                  lastDate: DateTime(2100),
-                ),
-              ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            TextFormField(
-              decoration: InputDecoration(hintText: 'Tipo de Producto'),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            TextFormField(
-              decoration: InputDecoration(hintText: 'Marca del Producto'),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            TextFormField(
-              decoration: InputDecoration(hintText: 'Cantidad del Producto'),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            TextFormField(
-              decoration: InputDecoration(hintText: 'Costo por unidad'),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            TextFormField(
-              decoration: InputDecoration(hintText: 'Costo Total'),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            Padding(
-              padding: EdgeInsets.only(
-                  top: MediaQuery.of(context).size.height * 0.01),
-              child: DropdownButtonFormField<String>(
-                decoration: InputDecoration(
-                  hintText: 'Selecciona un proveedor',
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(15),
-                  ),
-                ),
-                value: dropdownValue,
-                onChanged: (String? newValue) {
-                  setState(() {
-                    dropdownValue = newValue!;
-                  });
-                },
-                items: <String>[
-                  'Selecciona un proveedor',
-                  'Proveedor1',
-                  'Weon 2',
-                  'Weon3'
-                ].map<DropdownMenuItem<String>>((String value) {
-                  return DropdownMenuItem<String>(
-                    value: value,
-                    child: Text(value),
-                  );
-                }).toList(),
-              ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
             Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                ElevatedButton(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                    minimumSize: Size(
-                      MediaQuery.of(context).size.width * 0.6,
-                      MediaQuery.of(context).size.height * 0.07,
-                    ),
+                Padding(
+                  padding: EdgeInsets.only(
+                      top: MediaQuery.of(context).size.height * 0.02),
+                  child: TextField(
+                    controller: ordenIdController,
+                    decoration: const InputDecoration(hintText: 'ID de Orden'),
                   ),
-                  child: const Text('Realizar orden'),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(
+                      top: MediaQuery.of(context).size.height * 0.02),
+                  child: TextField(
+                    controller: fechaController,
+                    decoration:
+                        const InputDecoration(hintText: 'Fecha de Orden'),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(
+                      top: MediaQuery.of(context).size.height * 0.02),
+                  child: TextField(
+                    controller: cantidadController,
+                    decoration:
+                        const InputDecoration(hintText: 'Cantidad del Pedido'),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(
+                      top: MediaQuery.of(context).size.height * 0.02),
+                  child: TextField(
+                    controller: proveedorController,
+                    decoration:
+                        const InputDecoration(hintText: 'Seleccione Proveedor'),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(
+                      top: MediaQuery.of(context).size.height * 0.02),
+                  child: TextField(
+                    controller: costotalController,
+                    decoration: const InputDecoration(hintText: 'Costo Total'),
+                  ),
                 ),
               ],
             ),
-            const SizedBox(height: 10),
             Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.end,
               children: [
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => HomePage()),
-                    );
-                  },
-                  style: ElevatedButton.styleFrom(
-                    minimumSize: Size(
-                      MediaQuery.of(context).size.width * 0.6,
-                      MediaQuery.of(context).size.height * 0.07,
-                    ),
+                Padding(
+                  padding: EdgeInsets.only(
+                      top: MediaQuery.of(context).size.height * 0.02),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: ElevatedButton(
+                          onPressed: () {
+                            _saveData();
+                          },
+                          style: ElevatedButton.styleFrom(
+                            minimumSize: Size(
+                              (MediaQuery.of(context).size.width * 0.6),
+                              (MediaQuery.of(context).size.height * 0.07),
+                            ),
+                          ),
+                          child: const Text('Guardar'),
+                        ),
+                      ),
+                      const SizedBox(width: 20),
+                      Expanded(
+                        child: ElevatedButton(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => PurchaseList(),
+                              ),
+                            );
+                          },
+                          style: ElevatedButton.styleFrom(
+                            minimumSize: Size(
+                              (MediaQuery.of(context).size.width * 0.6),
+                              (MediaQuery.of(context).size.height * 0.07),
+                            ),
+                          ),
+                          child: const Text('Volver'),
+                        ),
+                      ),
+                    ],
                   ),
-                  child: const Text('Volver'),
                 ),
               ],
             ),
