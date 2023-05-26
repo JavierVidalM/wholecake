f83479497e528c8aed0d78c9f279eee66367da74
Nikolo <sanchez.nicolas.9917@gmail.com>
2023-05-26 00:12:30 -0400

    add orden de compra

--- lib/views/suppliers/suppliers_purchase_orders.dart
+++ lib/views/suppliers/suppliers_purchase_orders.dart
@@ -1,8 +1,10 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:wholecake/views/suppliers/suppliers.dart';
 import 'package:wholecake/views/utilities/sidebar.dart';
 
+import '../../models/suppliers.dart';
 import '../../services/productos_services.dart';
 
 class PurchaseOrders extends StatefulWidget {
@@ -17,6 +19,8 @@ class _PurchaseOrdersState extends State<PurchaseOrders> {
   TextEditingController cantidadController = TextEditingController();
   TextEditingController proveedorController = TextEditingController();
   TextEditingController costotalController = TextEditingController();
+  
+  get categoriaSeleccionada => null;
 
   @override
   void dispose() {
@@ -29,19 +33,23 @@ class _PurchaseOrdersState extends State<PurchaseOrders> {
 
   Future<void> _saveData() async {
     final msg = jsonEncode({
-      'Id': ordenIdController.text,
-      'Cantidad': cantidadController.text,
-      'Proveedor': proveedorController.text,
-      'Costo Total': costotalController.text,
+      'cantidad': cantidadController.text,
+      'proveedor': proveedorController.text,
+      'costotal': costotalController.text,
     });
     await ProductService().addOrdenCompra(msg);
-    print('saved');
     Navigator.push(
         context, MaterialPageRoute(builder: (context) => SuppliersView()));
   }
-
+  String? validateProveedor(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Por favor, seleccione un Proveedor.';
+    }
+    return null;
+  }
   @override
   Widget build(BuildContext context) {
+    final listaprov = Provider.of<ProductService>(context);
     return Scaffold(
       appBar: AppBar(
         title: const Text(
@@ -67,28 +75,33 @@ class _PurchaseOrdersState extends State<PurchaseOrders> {
                   padding: EdgeInsets.only(
                       top: MediaQuery.of(context).size.height * 0.02),
                   child: TextField(
-                    controller: ordenIdController,
-                    decoration: const InputDecoration(hintText: 'ID de Orden'),
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(
-                      top: MediaQuery.of(context).size.height * 0.02),
-                  child: TextField(
                     controller: cantidadController,
                     decoration:
                         const InputDecoration(hintText: 'Cantidad del Pedido'),
                   ),
                 ),
-                Padding(
-                  padding: EdgeInsets.only(
-                      top: MediaQuery.of(context).size.height * 0.02),
-                  child: TextField(
-                    controller: proveedorController,
-                    decoration:
-                        const InputDecoration(hintText: 'Seleccione Proveedor'),
+                  Padding(
+                    padding: EdgeInsets.only(
+                        top: MediaQuery.of(context).size.height * 0.02),
+                    child: DropdownButtonFormField<ListSup>(
+                      validator: (ListSup? value) =>
+                          validateProveedor(value?.nombreProveedor),
+                      hint: const Text('Selecciona un Proveedor'),
+                      value: categoriaSeleccionada,
+                      onChanged: (ListSup? nuevoSup) {
+                        setState(() {
+                          proveedorController.text =
+                              nuevoSup!.supplierId.toString();
+                        });
+                      },
+                      items: listaprov.listadosuppliers.map((proveedor) {
+                        return DropdownMenuItem<ListSup>(
+                          value: proveedor,
+                          child: Text(proveedor.nombreProveedor),
+                        );
+                      }).toList(),
+                    ),
                   ),
-                ),
                 Padding(
                   padding: EdgeInsets.only(
                       top: MediaQuery.of(context).size.height * 0.02),
